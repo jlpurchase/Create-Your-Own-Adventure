758b1391165f27ff7b063dd2bc0362e4c4832735
Jaynee <53342829+jlpurchase@users.noreply.github.com>
2020-02-25 09:59:28 -0600

    Add files via upload

---
+++ create your own adventure app.js
@@ -0,0 +1,19 @@
+//create your own adventure app
+
+
+function displayIntros (){
+    let scifiIntro = document.getElementById('scifiIntro')
+    let prehistoricIntro = document.getElementById('prehistoricIntro')
+    let magicIntro = document.getElementById('magicIntro')
+
+    if (checked === scifiIntro)
+        document.getElementById('scifiIntro');
+}
+// if Scifi is selecte display scifi intro
+
+//if Prehistoric is selected display Prehistoric intro
+
+//if Magic is selected display Magic intro
+
+//else display no intros
+
